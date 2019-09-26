de133c8b082b64d2550eef3cd52a6d51da6ba737
geek_rofusa <rofusa@geek.co.jp>
2019-09-26 15:17:27 +0900

    wip

---
+++ nuxt.config.js
@@ -0,0 +1,94 @@
+const baseDir = process.env.BASE_DIR || '/'
+
+export default {
+	mode: 'universal',
+	srcDir: 'src',
+	/*
+	 ** Headers of the page
+	 */
+	head: {
+		title: 'title text',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{
+				hid: 'description',
+				name: 'description',
+				content: 'description text'
+			}
+		],
+		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+	},
+	router: {
+		base: baseDir
+	},
+	/*
+	 ** nuxt generate settings
+	 */
+	generate: {
+		subFolders: false,
+		fallback: true
+	},
+	/*
+	 ** Global CSS
+	 */
+	css: ['@/assets/scss/base.scss'],
+
+	styleResources: {
+		scss: ['~/assets/scss/_various.scss']
+	},
+	/*
+	 ** Plugins to load before mounting the App
+	 */
+	plugins: [],
+	/*
+	 ** Nuxt.js dev-modules
+	 */
+	buildModules: [
+		// Doc: https://github.com/nuxt-community/eslint-module
+		'@nuxtjs/eslint-module',
+		'@nuxtjs/stylelint-module'
+	],
+	/*
+	 ** Nuxt.js modules
+	 */
+	modules: [
+		'@nuxtjs/pwa',
+		'@nuxtjs/sitemap',
+		'@nuxtjs/style-resources',
+		['vue-scrollto/nuxt', { duration: 500 }]
+	],
+	/*
+	 ** Build configuration
+	 */
+	build: {
+		/*
+		 ** You can extend webpack config here
+		 */
+		filenames: {
+			app: () => '[name].js',
+			chunk: () => '[name].js',
+			css: () => '[name].js',
+			img: () => '[path][name].[ext]',
+			font: () => '[path][name].[ext]',
+			video: () => '[path][name].[ext]'
+		},
+		postcss: {
+			preset: {
+				autoprefixer: {
+					grid: true
+				}
+			}
+		},
+		extend(config, { isDev, isClient }) {
+			if (isDev && isClient) {
+				config.module.rules.push({
+					enforce: 'pre',
+					test: /\.(js|vue)$/,
+					loader: 'eslint-loader',
+					exclude: /(node_modules)/
+				})
+			}
+		}
+	}
+}
