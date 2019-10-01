75a6142d3a033f99bf6c3de4068fb1a058a0f173
randy39 <3randy9@gmail.com>
2019-10-02 00:13:22 +0900

    [update] lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,9 +13,10 @@ module.exports = {
 		'prettier/vue',
 		'plugin:prettier/recommended',
 		'plugin:nuxt/recommended',
+		'plugin:vue/recommended',
 		'plugin:vue-a11y/recommended'
 	],
-	plugins: ['prettier', 'vue-a11y'],
+	plugins: ['vue', 'prettier', 'vue-a11y'],
 	// add your custom rules here
 	rules: {
 		'no-console': 'off',
@@ -25,6 +26,14 @@ module.exports = {
 				useTabs: true
 			}
 		],
-		'vue-a11y/alt-text': 2
+		'vue/html-indent': ['error', 'tab'],
+		'vue/html-self-closing': [
+			'error',
+			{
+				html: {
+					void: 'always'
+				}
+			}
+		]
 	}
 }
