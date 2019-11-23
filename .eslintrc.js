28e00c645b9b5ba6afb4cc2de6a701275f51eee6
randy39 <3randy9@gmail.com>
2019-11-23 19:40:02 +0900

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,39 @@
 module.exports = {
-	root: true,
-	env: {
-		browser: true,
-		node: true
-	},
-	parserOptions: {
-		parser: 'babel-eslint'
-	},
-	extends: [
-		'@nuxtjs',
-		'prettier',
-		'prettier/vue',
-		'plugin:prettier/recommended',
-		'plugin:nuxt/recommended',
-		'plugin:vue/recommended',
-		'plugin:vue-a11y/recommended'
-	],
-	plugins: ['vue', 'prettier', 'vue-a11y'],
-	// add your custom rules here
-	rules: {
-		'no-console': 'off',
-		'prettier/prettier': [
-			'error',
-			{
-				useTabs: false
-			}
-		],
-		'vue/html-self-closing': [
-			'error',
-			{
-				html: {
-					void: 'always'
-				}
-			}
-		]
-	}
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    '@nuxtjs',
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended',
+    'plugin:vue/recommended',
+    'plugin:vue-a11y/base',
+    'plugin:vue-a11y/recommended'
+  ],
+  plugins: ['vue', 'prettier', 'vue-a11y'],
+  // add your custom rules here
+  rules: {
+    'no-console': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        useTabs: false
+      }
+    ],
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always'
+        }
+      }
+    ]
+  }
 }
